359eeb99ec8c0dbb0889838f8f37ea82bc820bc5
G4rutti <davigarutti5@gmail.com>
2023-11-09 09:15:11 -0300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function empresario(){
+    window.location.href = "EmpresaLogin.html"
+}
+
+function aluno(){
+    window.location.href = "alunoLogin.html"
+}
+
+function salaDeCursos(){
+    window.location.href = "coursesPages.html"
+}
